6aedc2c6b2a88020851448ba2ad6a6816e2bb779
aalexcn <a26516@sanvalero.net>
2023-09-27 12:23:06 +0200

    netflix

--- Trabajo 1/android/app/src/main/java/com/example/a1_appandoid/MainActivity.java
+++ Trabajo 1/android/app/src/main/java/com/example/a1_appandoid/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.a1_appandoid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUser;
+    private EditText edtPassword;
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUser = findViewById(R.id.edtUser);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+btnSignIn.setOnClickListener(new MiClaseClick());
+
     }
 }
---
+++ Trabajo 1/android/app/src/main/java/com/example/a1_appandoid/MiClaseClick.java
@@ -0,0 +1,11 @@
+package com.example.a1_appandoid;
+
+import android.view.View;
+
+public class MiClaseClick implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+};
+
