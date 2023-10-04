b1fc6cb1e06dcffd9bce063d0a5ec1faee20832e
aalexcn <129084470+aalexcn@users.noreply.github.com>
2023-10-04 10:37:52 +0200

    login y medio java

--- login/app/src/main/java/com/example/login03102023/MainActivity.java
+++ login/app/src/main/java/com/example/login03102023/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.login03102023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+            private EditText edtEmail;
+            private EditText edtPassword;
+            private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        initComponents();
+    }
+    private void initComponents(){
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
